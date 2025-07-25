5f6150b9ff3a1ef2dbd1ab9128397ed41ca95fdd
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-07-26 11:36:39 +1200

    Bubble falls towards center and then gets stuck

--- Scenes/bubble.gd
+++ Scenes/bubble.gd
@@ -1,7 +1,30 @@
 extends RigidBody2D
 
+@export var our_gravity_scale: float = 0.1;
+
+enum BubbleState { Falling, Stuck }
+
+var _currentState: BubbleState
+
 func _enter_tree():
 	body_entered.connect(on_collision)
+	
+	set_state(BubbleState.Falling)
+
+func _process(delta):
+	# Center is 0, 0
+	var vector_to_center = -1 * global_position
+	
+	if _currentState == BubbleState.Falling:
+		add_constant_force(vector_to_center * our_gravity_scale)
+
+func set_state(state: BubbleState):
+	_currentState = state
+	
+	match state:
+		BubbleState.Stuck:
+			set_constant_force(Vector2.ZERO)
+			set_constant_torque(0)
 
 func on_collision(body: Node):
-	pass
+	set_state(BubbleState.Stuck)
