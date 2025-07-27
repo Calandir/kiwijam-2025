b97560b9223b1982db888a97d568ede9e42b0641
Kilreth <jonathan.pavat.brown@gmail.com>
2025-07-27 14:15:33 +1200

    Use correct button signal for quit to title

--- Scripts/quit_to_title.gd
+++ Scripts/quit_to_title.gd
@@ -1,6 +1,6 @@
 extends Button
 
 
-func _on_button_down() -> void:
+func _on_pressed() -> void:
 	var game_scene = load("res://Scenes/tash_menu.tscn")
 	get_tree().change_scene_to_packed(game_scene)
