c0a387827a7e97a8eccbda0eb01a2749b6a1b54d
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-07-28 12:51:08 +1200

    Hook up more debug spawn types

--- Scripts/debug_input_listener.gd
+++ Scripts/debug_input_listener.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @export var _spawn_template_red: PackedScene
 @export var _spawn_template_blue: PackedScene
+@export var _spawn_template_green: PackedScene
+@export var _spawn_template_yellow: PackedScene
+@export var _spawn_template_purple: PackedScene
+@export var _spawn_template_brown: PackedScene
 
 @export var _spawn_container: Node2D
 
@@ -11,6 +15,18 @@ func _process(delta):
 		
 	if Input.is_action_just_pressed("debug_spawn_bubble_blue"):
 		_instantiate(_spawn_template_blue)
+		
+	if Input.is_action_just_pressed("debug_spawn_bubble_green"):
+		_instantiate(_spawn_template_green)
+		
+	if Input.is_action_just_pressed("debug_spawn_bubble_yellow"):
+		_instantiate(_spawn_template_yellow)
+		
+	if Input.is_action_just_pressed("debug_spawn_bubble_purple"):
+		_instantiate(_spawn_template_purple)
+		
+	if Input.is_action_just_pressed("debug_spawn_bubble_brown"):
+		_instantiate(_spawn_template_brown)
 
 func _instantiate(template: PackedScene):
 	var spawned: Node2D = template.instantiate()
