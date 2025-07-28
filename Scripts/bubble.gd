6af6cb1690e9fafe3e0be0d735bb876cb5a41ca6
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-07-28 13:05:25 +1200

    Clean up old code

--- Scripts/bubble.gd
+++ Scripts/bubble.gd
@@ -20,10 +20,8 @@ enum BubbleType
 enum BubbleState { Falling, Stuck }
 
 var _currentState: BubbleState
-var _overflowBorder: Area2D
 var _sfxPlayer: Node
 
-var _spawnFrame: int
 var _lastStateChangedFrame: int = -1
 
 var _needs_physics_reset: bool = false
@@ -38,14 +36,9 @@ func _ready():
 	if not s_center:
 		s_center = get_tree().current_scene.find_child("Center", true)
 	
-	_spawnFrame = Engine.get_frames_drawn()
 	set_state(BubbleState.Falling, null)
 
 func _process(delta):
-	# Hack to clean up distant satellites
-	if Engine.get_frames_drawn() > _spawnFrame + 300 and position.length_squared() > 1200 * 1200:
-		queue_free()
-	
 	# Center is 0, 0
 	var vector_to_center = -1 * global_position
 	
@@ -62,7 +55,6 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 		angular_velocity = 0
 		# Hack, bubble needs to get going faster
 		our_gravity_scale = 0.5
-		
 	
 func set_state(state: BubbleState, new_parent: Node2D):
 	var previous_state: BubbleState = _currentState
@@ -138,49 +130,6 @@ func on_collision(body: Node):
 	var game_state = get_tree().current_scene.find_child("GameState", true)
 	if game_state != null:
 		game_state.add_score(len(all_matches) * len(all_matches))
-# OLD STUFF BELOW
-
-func on_collision_old(body: Node):
-	if _currentState == BubbleState.Stuck:
-		return
-	
-	var has_hit_center = body is StaticBody2D
-	var has_hit_stuck_bubble = body is Bubble and body._currentState == BubbleState.Stuck
-	
-	if has_hit_center or has_hit_stuck_bubble:
-		# call_deferred() required to freeze physics at end of frame
-		(func(): self.set_state(BubbleState.Stuck, body)).call_deferred()
-		
-		var matching_neighbors = _get_bubble_matching_neighbors()
-		
-		var all_connected: Array[Bubble] = matching_neighbors.duplicate()
-		var all_matches: Array[Bubble] = matching_neighbors.duplicate()
-		all_matches.append(self)
-		
-		for x in matching_neighbors:
-			x._add_deeper_connections(all_connected, func(bubble): return true)
-			x._add_deeper_connections(all_matches, func(bubble): return bubble.type == self.type)
-		
-		if len(all_matches) < 3:
-			if has_hit_center and len(all_matches) == 1:
-				_sfxPlayer.play_miss_sfx()
-			else:
-				_sfxPlayer.play_connect_sfx()
-			return
-		
-		_sfxPlayer.play_spell_sfx()
-
-		var orphaned = all_connected.filter(func(x): return not all_matches.has(x))
-		for orphan: Bubble in orphaned:
-			(func(): orphan.set_state(BubbleState.Falling, get_tree().current_scene)).call_deferred()
-			
-		for x in all_matches:
-			x._play_effect()
-			x.queue_free()
-		
-		var game_state = get_tree().current_scene.find_child("GameState", true)
-		if game_state != null:
-			game_state.add_score(len(all_matches) * len(all_matches))
 
 func _play_effect():
 	if not is_inside_tree():
@@ -192,40 +141,3 @@ func _play_effect():
 		get_tree().current_scene.add_child(new)
 	
 	queue_free()
-
-func _get_bubble_matching_neighbors() -> Array[Bubble]:
-	var neighbors = _connectionHitbox.get_overlapping_bodies()
-	
-	var results: Array[Bubble] = []
-	
-	for neighbor in neighbors:
-		if neighbor == self:
-			continue
-		
-		var as_bubble = neighbor as Bubble
-		if as_bubble == null or as_bubble.type != self.type:
-			continue
-		
-		results.append(neighbor)
-	
-	return results
-
-func _add_deeper_connections(already_matched: Array[Bubble], is_valid_check: Callable):
-	if not already_matched.has(self):
-		already_matched.append(self)
-	
-	var bubble_children = get_children().filter(func(x): return x as Bubble != null)
-	
-	for bubble in bubble_children:
-		if not is_valid_check.call(bubble):
-			continue
-		
-		if not already_matched.has(bubble):
-			already_matched.append(bubble)
-			bubble._add_deeper_connections(already_matched, is_valid_check)
-	
-	var parent = get_parent()
-	var parent_as_bubble = parent as Bubble
-	if parent_as_bubble != null and (not already_matched.has(parent)) and is_valid_check.call(parent_as_bubble):
-		already_matched.append(parent)
-		parent._add_deeper_connections(already_matched, is_valid_check)
