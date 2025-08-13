01feed4237cdbf19f67fe6ae78f8cf19c27bc62d
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-08-13 20:44:39 +1200

    Add arrow to indicator

--- Scripts/indicator.gd
+++ Scripts/indicator.gd
@@ -2,9 +2,10 @@ class_name Indicator
 extends Node2D
 
 @export var _sprite2D: Sprite2D
+@export var _arrow: Node2D
 
-static var min_corner: Vector2 = Vector2(-1200, -1200)
-static var max_corner: Vector2 = Vector2(1200, 1200)
+static var min_corner: Vector2 = Vector2(-1100, -1100)
+static var max_corner: Vector2 = Vector2(1100, 1100)
 
 var _bubble: Node2D
 
@@ -23,3 +24,5 @@ func _process(delta: float) -> void:
 	
 	var clamped_position = _bubble.global_position.clamp(min_corner, max_corner)
 	self.global_position = clamped_position
+	
+	_arrow.look_at(_bubble.global_position)
