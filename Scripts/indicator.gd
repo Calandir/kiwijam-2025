b1a9da7bbb7e4e9aad276381774199af5a44fe4e
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-08-13 21:00:49 +1200

    Indicator arrow always points directly to an edge (no weird angles)

--- Scripts/indicator.gd
+++ Scripts/indicator.gd
@@ -25,4 +25,20 @@ func _process(delta: float) -> void:
 	var clamped_position = _bubble.global_position.clamp(min_corner, max_corner)
 	self.global_position = clamped_position
 	
-	_arrow.look_at(_bubble.global_position)
+	var position_diff: Vector2 = _bubble.global_position - clamped_position
+	var angle_degrees = rad_to_deg(position_diff.normalized().angle())
+	
+	# Ensure the arrow always points to an edge
+	angle_degrees = _get_closest_degrees(angle_degrees)
+	
+	_arrow.rotation_degrees = angle_degrees
+
+func _get_closest_degrees(input_degrees: float) -> float:
+	var to_compare: Array[float] = [-180, -90, 0, 90, 180]
+	var current_best = -180
+	
+	for num in to_compare:
+		if abs(num - input_degrees) < abs(current_best - input_degrees):
+			current_best = num
+	
+	return current_best
