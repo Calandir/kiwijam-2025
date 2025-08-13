b20f2a8a66289c5e56ec50e1a2c321477cd2e3f2
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-08-13 20:03:09 +1200

    Set indicator image and clamp its position

--- Scripts/indicator.gd
+++ Scripts/indicator.gd
@@ -1,10 +1,17 @@
 class_name Indicator
 extends Node2D
 
+@export var _sprite2D: Sprite2D
+
+static var min_corner: Vector2 = Vector2(-1200, -1200)
+static var max_corner: Vector2 = Vector2(1200, 1200)
+
 var _bubble: Node2D
 
 func set_bubble(bubble: Node2D) -> void:
 	_bubble = bubble
+	_sprite2D.texture = bubble.find_child("Sprite2D").texture
+	
 	self.global_position = _bubble.global_position
 
 func clear_bubble() -> void:
@@ -14,4 +21,5 @@ func _process(delta: float) -> void:
 	if not _bubble:
 		return
 	
-	self.global_position = _bubble.global_position
+	var clamped_position = _bubble.global_position.clamp(min_corner, max_corner)
+	self.global_position = clamped_position
